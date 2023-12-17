218755c4902972a1675e30490910de54ca9da859
w72 <w72e@qq.com>
2023-12-18 01:15:06 +0800

    style: fix compose code style

--- src/utils/compose.ts
+++ src/utils/compose.ts
@@ -3,12 +3,15 @@ import { Context, Middleware } from "../types";
 export function compose(middleware: Middleware[]) {
   return async (ctx: Context): Promise<unknown> => {
     let ret: unknown;
+
     const dispatch = async (i: number) => {
       const fn = middleware[i];
       const res = await fn(ctx, dispatch.bind(null, i + 1));
       if (i === middleware.length - 1) ret = res;
     };
+
     await dispatch(0);
+
     return ret;
   };
 }
